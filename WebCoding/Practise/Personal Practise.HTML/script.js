1a7f4b69ea0ed016bb816256b88cffce9767af1c
Ayush <abhuiyanshrizoy@gmail.com>
2023-03-28 20:37:16 +0600

    new html added

--- WebCoding/Practise/Personal Practise.HTML/script.js
+++ WebCoding/Practise/Personal Practise.HTML/script.js
@@ -1,1 +1,9 @@
 console.log("Hello word");
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
