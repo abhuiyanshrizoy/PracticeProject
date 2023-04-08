95f46d9ccc3f909b073a65776d20976af90311e5
Ayush <abhuiyanshrizoy@gmail.com>
2023-04-08 21:22:13 +0600

    stopwatch

---
+++ WebCoding/Practise/java.ninja/STOPWATCH/script.js
@@ -0,0 +1,61 @@
+var h = 0;
+var m = 0;
+var s = 0;
+var stopTime = true;
+
+function startTimer()
+{
+    if(stopTime == true)
+    {
+        stopTime = false;
+        startOn();
+    }
+}
+
+function startOn()
+{
+    if(stopTime == false) {
+        h = parseInt(h);
+        m = parseInt(m);
+        s = parseInt(s);
+
+        s = s + 1;
+        if(s == 60)
+        {
+            m = m + 1;
+            s = 0;
+        }
+        if(m == 60)
+        {
+            h = h + 1;
+            s = 0;
+            m = 0;
+        }
+        if(h < 10){
+            h = "0" + h;
+        }
+        if(m < 10){
+            m = "0" + m;
+        }
+        if(s < 10){
+            s = "0" + s;
+        }
+        document.getElementById('stopwatch').innerText = h + ':' + m + ':' + s;
+
+        setTimeout("startOn()", 200);
+    }
+}
+
+function stopTimer(){
+    if(stopTime == false){
+        stopTime = true;
+    }
+}
+
+function resetTimer(){
+    document.getElementById('stopwatch').innerText ="00:00:00"; 
+     h = 0;
+     m = 0;
+     s = 0;
+     stopTime = true;
+}
