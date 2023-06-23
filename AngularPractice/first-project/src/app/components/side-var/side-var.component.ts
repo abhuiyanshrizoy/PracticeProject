59e3c87a579cbc8b63f4b168deff8ff0adbf00c2
Ayush <abhuiyanshrizoy@gmail.com>
2023-06-23 20:22:23 +0600

    Initial Commit

---
+++ AngularPractice/first-project/src/app/components/side-var/side-var.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-var',
+  templateUrl: './side-var.component.html',
+  styleUrls: ['./side-var.component.css']
+})
+export class SideVarComponent {
+
+}
